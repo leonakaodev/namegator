28bedfd9d945e3b826b1898a76316235ccfebc22
= <=>
2020-03-19 12:28:09 -0300

    Adiciona o Vuex na aplicação

--- client/src/components/DomainList.vue
+++ client/src/components/DomainList.vue
@@ -41,125 +41,27 @@
 </template>
 
 <script>
+import { mapState, mapActions} from "vuex";
 import AppItemList from "./AppItemList";
-import axios from "axios/dist/axios";
 
 export default {
 	name: "DomainList",
 	data: function(){
-		return {
-			items: {
-				prefix: [],
-				sufix:[],
-			},
-			domains: [],
-		};
+		return {};
 	},
 	components: {
 		AppItemList
 	},
 	methods: {
-		addItem: function(item){
-			axios({
-				url: "http://localhost:4000",
-				method: "post",
-				data: {
-					query: `
-						mutation ($item: ItemInput) {
-							newItem: saveItem(item: $item) {
-								id,
-								type,
-								description
-							}
-						}
-					`,
-					variables: {
-						item
-					}
-				}
-			}).then(response => {
-				this.items[item.type].push(response.data.data.newItem);
-				this.generateDomains();
-			});
-		},
-		deleteItem: function(item){
-			axios({
-				url: "http://localhost:4000",
-				method: "post",
-				data: {
-					query: `
-						mutation ($id: Int) {
-							deleted: deleteItem(id: $id)
-						}
-					`,
-					variables: {
-						id: item.id
-					}
-				}
-			}).then(response => {
-				const query = response.data;
-				if(query.data.deleted){
-					this.items[item.type].splice(this.items[item.type].indexOf(item), 1);
-					this.generateDomains();
-				}
-			});
-		},
-		getItems(type){
-			return axios({
-				url: "http://localhost:4000",
-				method: "post",
-				data: {
-					query: `
-						query ($type: String){
-							items: items (type: $type) {
-								id
-								type
-								description
-							}
-						}
-					`,
-					variables: {
-						type
-					}
-				}
-			}).then(response => {
-				const query = response.data;
-				this.items[type] = query.data.items;
-			});
-		},
-		generateDomains(){
-			axios({
-				url: "http://localhost:4000",
-				method: "post",
-				data: {
-					query: `
-						mutation {
-							domains: generateDomains {
-								name
-								checkout
-								available
-							}
-						}
-					`
-				}
-			}).then(response => {
-				const query = response.data;
-				this.domains = query.data.domains;
-			});
-		},
+		...mapActions(["addItem", "deleteItem", "getItems", "generateDomains"]),
 		openDomain(domain) {
 			this.$router.push({
 				path: `/domains/${domain.name}`,
 			});
 		}
 	},
-	created(){
-		Promise.all([
-			this.getItems("prefix"),
-			this.getItems("sufix")
-		]).then(() => {
-			this.generateDomains();
-		});
+	computed: {
+		...mapState(["items", "domains"])
 	}
 };
 </script>
